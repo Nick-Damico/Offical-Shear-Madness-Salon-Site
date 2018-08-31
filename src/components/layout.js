b5c1d4b957647238249d399a12d48b41dc2d9ce9
Nick-Damico <nickalan82@icloud.com>
2018-08-31 13:31:27 -0400

    renamed layout.js to old-layout.js for reference only, add new /layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,19 @@
-// import React from 'react'
-//
-// import Header from './header'
-// import './layout.css'
-//
-// const Layout = ({ children }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query SiteTitleQuery {
-//         site {
-//           siteMetadata {
-//             title
-//           }
-//         }
-//       }
-//     `}
-//     render={data => (
-//       <>
-//         <Helmet
-//           title={data.site.siteMetadata.title}
-//           meta={[
-//             { name: 'description', content: 'Sample' },
-//             { name: 'keywords', content: 'sample, something' },
-//           ]}
-//         >
-//           <html lang="en" />
-//         </Helmet>
-//         <Header siteTitle={data.site.siteMetadata.title} />
-//         <div
-//           style={{
-//             margin: '0 auto',
-//             maxWidth: 960,
-//             padding: '0px 1.0875rem 1.45rem',
-//             paddingTop: 0,
-//           }}
-//         >
-//           {children}
-//         </div>
-//       </>
-//     )}
-//   />
-// )
-//
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
-//
-// export default Layout
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  background-color: tomato;
+   flex-direction: column;
+   align-items: center;
+   margin: 0 auto 12px auto;
+   &:last-child {
+     margin-bottom: 0;
+   }
+`
+
+export default ({ children }) => (
+  <Wrapper>
+    { children }
+  </Wrapper>
+);
