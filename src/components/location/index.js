7364198ada6959e2f2dd6e5ac40924b45162a2af
Nick-Damico <nickalan82@icloud.com>
2018-09-07 12:51:19 -0400

    import Info component and LocationPic, styled child components added to return block on component

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import Info from './info';
+
+import LocationPic from '../../images/salonLocation.jpg';
 
 const LocationContainer = styled.section`
-  min-height: 400px;
+  display: flex;
+  flex-direction: column;  
+
+  @media (min-width: 499px) {
+    height: 500px;
+    flex-direction: row;
+  }
 `;
 
-const Row = styled.div`
+const PhotoBlock = styled.div`
+  display: none;
 
+  @media (min-width: 499px) {
+    display: block;
+    width: 50%;
+    background-color: ${props => props.theme.secondaryColor};
+    background: ${props => props.theme.transparentGradient}, url(${LocationPic}) no-repeat center;
+    background-size: cover;
+  }
 `;
 
 export default () => {
   return (
     <LocationContainer>
-      <h2>Location</h2>
-      <Row className="row">
-        
-      </Row>
+      <Info />
+      <PhotoBlock />
     </LocationContainer>
   );
 };
