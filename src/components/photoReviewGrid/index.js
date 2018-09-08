453aa86b6f820ab073742d0de460b11fd9718ba9
Nick-Damico <nickalan82@icloud.com>
2018-09-08 18:36:39 -0400

    add reviewCard.js, styled component, imported into photoReviewGrid/index.js to handle format of user reviews

--- src/components/photoReviewGrid/index.js
+++ src/components/photoReviewGrid/index.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import Pic from '../../images/pic.jpg';
+
+import ReviewCard from './reviewCard';
 
 const GridContainer = styled.section`
 
   display: grid;
   background-color: ${props => props.theme.colorWhite};
-  grid-column-gap: 20px;
-  grid-row-gap: 24px;
-  grid-template-columns: repeat(2, 1fr);
+
+  grid-template-columns: auto;
   grid-template-rows: auto;
 
-  .div-1 {
-    grid-column: 1 / span 1;
-    grid-rows: 1 / span 1;
+  [class*=div-] {
     width: 100%;
     height: 100%;
   }
 
+  @media (min-width: 499px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: auto;
+  }
+
   @media (min-width: 899px) {
+    grid-column-gap: 20px;
+    grid-row-gap: 24px;
     grid-template-rows: repeat(6, 1fr);
     grid-template-columns: 3fr 3fr 2fr 4fr;
     min-height: 580px;
@@ -28,6 +35,7 @@ const GridContainer = styled.section`
       grid-row: 1 / span 3;
       width: 100%;
       height: 100%;
+      background-color: ${props => props.theme.secondaryColor};
     }
 
     .div-2 {
@@ -49,6 +57,7 @@ const GridContainer = styled.section`
       grid-row: 1 / span 3;
       width: 100%;
       height: 100%;
+      background-color: ${props => props.theme.primaryColor};
     }
 
     .div-5 {
@@ -88,10 +97,23 @@ const GridContainer = styled.section`
 export default () => {
   return (
     <GridContainer>
-      <div className="div-1"></div>
+      <ReviewCard className="div-1"
+        profilePic={Pic}
+        username='Teka Riddle'
+        review='I am very weird about who touches my hair, but I was recommended
+        here by a friend and to Cassie. I absolutely loved my entire experience.
+        Truly remarkable, I recommend 110% to anyone. Sincerely A girl who loves her hair!!!'
+      />
+      <ReviewCard className="div-4"
+        profilePic={Pic}
+        username='Teka Riddle'
+        review='I am very weird about who touches my hair, but I was recommended
+        here by a friend and to Cassie. I absolutely loved my entire experience.
+        Truly remarkable, I recommend 110% to anyone. Sincerely A girl who loves her hair!!!'
+      />
       <div className="div-2"></div>
       <div className="div-3"></div>
-      <div className="div-4"></div>
+
       <div className="div-5"></div>
       <div className="div-6"></div>
       <div className="div-7"></div>
