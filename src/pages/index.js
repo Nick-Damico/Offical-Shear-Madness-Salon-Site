eb5387b1d325cbe628d662ecb671a2d8aa5fed49
Nick-Damico <nickalan82@icloud.com>
2019-02-15 10:26:15 -0500

    Add GoogleMap Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,36 @@
-import React from 'react';
+import React from 'react'
 // Import styled-components Package
-import styled from "styled-components";
-import '../css/globalStyles.js';
+import styled from 'styled-components'
+import '../css/globalStyles.js'
 
 // Import Components
-import Layout from '../components/layout';
-import { Navigation } from '../components/navigation';
-import Header from '../components/header/index';
-import About from '../components/about/index';
-import PhotoReviews from '../components/photoReviewGrid/index';
-import Services from '../components/services/index';
-import Location from '../components/location/index';
-import Footer from '../components/footer/index';
-
+import Layout from '../components/layout'
+import { Navigation } from '../components/navigation'
+import Header from '../components/header/index'
+import About from '../components/about/index'
+import PhotoReviews from '../components/photoReviewGrid/index'
+import Services from '../components/services/index'
+import Location from '../components/location/index'
+import Footer from '../components/footer/index'
+import GoogleMap from '../components/GoogleMap'
 
 const Main = styled.main`
   display: flex;
   flex-direction: column;
   width: 100%;
-`;
+`
 
 export default () => (
   <Layout>
     <Navigation />
     <Header />
-    <Main >
+    <Main>
       <About />
       <PhotoReviews />
       <Services />
       <Location />
+      <GoogleMap />
       <Footer />
     </Main>
   </Layout>
-);
+)
