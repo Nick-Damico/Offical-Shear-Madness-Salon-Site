6748547b2a5a78aba048dac76ca0d729b6b917f8
Nick-Damico <nickalan82@icloud.com>
2019-03-05 20:25:09 -0500

    Restructuring as Class Component

--- src/components/gallery/GalleryItem.js
+++ src/components/gallery/GalleryItem.js
@@ -3,13 +3,25 @@ import styled from 'styled-components'
 
 const GalleryImg = styled.img`
   width: 100%;
+  display: flex;
+  opacity: 0.8;
+  transform: opacity 0.8s ease-in-out;
+
+  &:hover {
+    opacity: 1;
+  }
 `
 
 const GalleryItem = styled.a`
+  background-color: ${props => props.theme.primaryColor};
+
+  &:nth-child(even) {
+    background-color: ${props => props.theme.secondaryColor};
+  }
 `
 
 export default ({ imageSml, imageSml2x, imageLg, imageLg2x, alt }) => {
-  
+
   return (
     <GalleryItem
       className="col"
