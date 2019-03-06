8d34a2357e1424fadd8ac56c24efb0cabbaeaaac
Nick-Damico <nickalan82@icloud.com>
2019-03-06 13:10:52 -0500

    move StylistCard.js rendering to a parent container element StylistCardContainer.js out of previous index.js, about/index.js single responsibility is to render all components for the about section.

---
+++ src/components/about/StylistCardContainer.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components'
+import StylistCard from './stylistCard'
+import StylistPic from '../../images/stylist.png'
+
+const StylistContainer = styled.div`
+  background-color: ${props => props.theme.primaryColor};
+
+  @media (min-width: 699px) {
+    background-color: transparent;
+  }
+`
+
+const TitleContainer = styled.div`
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto 80px auto;
+  padding: 1%;
+  text-align: center;
+
+  @media (min-width: 699px) {
+    text-align: left;
+  }
+`
+
+const SectionTitle = styled.h3`
+  color: blue;
+  font-size: 2.2rem;
+  color: ${props => props.theme.colorWhite};
+`
+
+const CardContainer = styled.div`
+  justify-content: space-around;
+  &:first-child {
+    margin-bottom: 50px;
+  }
+  &:last-child {
+    background-color: ${props => props.theme.primaryColor};
+  }
+
+  @media (min-width: 699px) {
+    &:last-child {
+      background-color: transparent;
+    }
+  }
+`
+
+const StylistCardContainer = ({ data }) => {
+  return(
+    <StylistContainer>
+      <TitleContainer>
+        <SectionTitle>Stylists</SectionTitle>
+      </TitleContainer>
+      <CardContainer className="row">
+        {data.allStylistsJson.edges.map((edge,index) => (
+          <StylistCard
+            key={index}
+            profilePic={StylistPic}
+            name={edge.node.name}
+            title={edge.node.title}
+          />
+        ))}
+      </CardContainer>
+    </StylistContainer>
+  )
+}
+
+export default StylistCardContainer
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { StaticQuery } from 'gatsby'
 import AboutIntro from './AboutIntro'
 import ServiceCardContainer from './ServiceCardContainer'
+import StylistCardContainer from './StylistCardContainer'
 import StylistCard from './stylistCard'
 import StylistPic from '../../images/stylist.png'
 import HairdryerSvg from '../../images/hairdryer-graphic.svg'
@@ -23,48 +24,6 @@ const AboutSection = styled.section`
   }
 `
 
-const StylistContainer = styled.div`
-  background-color: ${props => props.theme.primaryColor};
-
-  @media (min-width: 699px) {
-    background-color: transparent;
-  }
-`
-
-const TitleContainer = styled.div`
-  width: 100%;
-  max-width: 900px;
-  margin: 0 auto 80px auto;
-  padding: 1%;
-  text-align: center;
-
-  @media (min-width: 699px) {
-    text-align: left;
-  }
-`
-
-const SectionTitle = styled.h3`
-  color: blue;
-  font-size: 2.2rem;
-  color: ${props => props.theme.colorWhite};
-`
-
-const CardContainer = styled.div`
-  justify-content: space-around;
-  &:first-child {
-    margin-bottom: 50px;
-  }
-  &:last-child {
-    background-color: ${props => props.theme.primaryColor};
-  }
-
-  @media (min-width: 699px) {
-    &:last-child {
-      background-color: transparent;
-    }
-  }
-`
-
 export default () => (
   <StaticQuery
     query={graphql`
@@ -74,8 +33,6 @@ export default () => (
             node {
               name
               title
-              bio
-              skills
             }
           }
         }
@@ -86,23 +43,7 @@ export default () => (
         <AboutIntro />
         <AboutSection>
         <ServiceCardContainer />
-          <StylistContainer>
-            <TitleContainer>
-              <SectionTitle>Stylists</SectionTitle>
-            </TitleContainer>
-            <CardContainer className="row">
-              {data.allStylistsJson.edges.map((edge,index) => (
-                <StylistCard
-                  key={index}
-                  profilePic={StylistPic}
-                  name={edge.node.name}
-                  title={edge.node.title}
-                  bio={edge.node.bio}
-                  skills={edge.node.skills}
-                />
-              ))}
-            </CardContainer>
-          </StylistContainer>
+        <StylistCardContainer data={data} />
         </AboutSection>
       </About>
     )}
